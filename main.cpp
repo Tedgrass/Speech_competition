4582d10a09b4c345fe7bda1cb8bb0b667f90810d
Laptop_company <446884764@qq.com>
2022-08-31 23:14:01 +0800

    v0.2

--- main.cpp
+++ main.cpp
@@ -63,104 +63,104 @@ void savePerson(const vector<person> &v1){
 	fs.close();
 }
 
-class speechCompetation {
-public:
-	
-	speechCompetation(){
-		fstream fs;
-		fs.open("比赛记录.csv",ios::in);
-		string num;
-		getline(fs,num);
-		this->numofCom = num[0]-48;
-		for(int i=0;i<4*this->numofCom;i++){
-			//TODO
-			string temp;
-			getline(fs,temp);
-			if((i+4)%4==0) continue;
-			for(int i=11;i<12;i++){
-				//TODO
-				cout<<temp[i]<<endl;
-			}
-			for(int i=12;i<temp.size();i++){
-				//TODO
-				cout<<temp[i]<<endl;
-			}
-		}
-		fs.close();
-	}
-
-	void competationStart() {
-		vector<person> participants;
-		personPrepare(participants);
-		cout << "本届演讲比赛开始" << endl;
-		cout << "-----------------------------------------" << endl;
-		cout << "第一轮参赛选手出厂顺序如下：" << endl;
-		random_shuffle(participants.begin(), participants.end());
-		for_each(participants.begin(), participants.end(), printPerson);
-		cout << "----------------------------------------" << endl;
-		cout << "-------------比赛进行中-----------------" << endl;
-		cout << "----------------------------------------" << endl;
-		system("pause");
-		cout << "----------------------------------------" << endl;
-		for_each(participants.begin(), participants.end(), setScore);
-		cout << "第一组排名顺序如下：" << endl;
-		sort(participants.begin(), participants.begin() + 6,comparePerson);
-		for_each(participants.begin(), participants.begin() + 6,printPerson);
-		cout << "-----------------------------------------" << endl;
-		cout << "第二组排名顺序如下：" << endl;
-		sort(participants.begin() + 6, participants.end(), comparePerson);
-		for_each(participants.begin() + 6, participants.end(), printPerson);
-		cout << "----------------------------------------" << endl;
-		cout << "-------------第二轮比赛进行中-----------" << endl;
-		cout << "----------------------------------------" << endl;
-		system("pause");
-		participants.erase(participants.begin() + 9, participants.end());
-		participants.erase(participants.begin() + 3, participants.begin()+6);
-		for_each(participants.begin(), participants.end(), setScore);
-		sort(participants.begin(), participants.end(), comparePerson);
-		for_each(participants.begin(), participants.end(), printPerson);
-		
-		cout << "----------------------------------------" << endl;
-		cout << "比赛结束，名次如下：" << endl;
-		cout << "冠军是：" << participants[0].m_name << "\t得分：" << participants[0].m_score<<endl;
-		cout << "亚军是：" << participants[1].m_name << "\t得分：" << participants[1].m_score<<endl;
-		cout << "季军是：" << participants[2].m_name << "\t得分：" << participants[2].m_score<<endl;
-		cout << "----------------------------------------" << endl;
-		
-		for (vector<person>::iterator it = participants.begin(); it != participants.end()-3;it++) {
-			this->Honor.push_back(*it);
-		}
-		this->numofCom++;
-		savePerson(this->Honor);
-	}
-	
-	void lookup() {
-		
-		if (this->numofCom == 0) return ;
-		for (int i = 0; i < this->numofCom; i++)
-		{
-			cout << "第" << i+1 << "届演讲比赛名次如下：" << endl;
-			cout << "冠军是：" << this->Honor[0 + i * 3].m_name << "\t得分：" << this->Honor[0 + i * 3].m_score << endl;
-			cout << "亚军是：" << this->Honor[1 + i * 3].m_name << "\t得分：" << this->Honor[1 + i * 3].m_score << endl;
-			cout << "季军是：" << this->Honor[2 + i * 3].m_name << "\t得分：" << this->Honor[2 + i * 3].m_score << endl;
-		}
-	}
-	
-	void clearall() {
-		cout << "是否清空比赛记录？" << endl;
-		int choice;
-		cin >> choice;
-		if (choice) {
-			this->Honor.clear();
-			savePerson(this->Honor);
-			this->numofCom=0;
-		}
-	
-	}
-public:
-	vector<person> Honor;
-	int numofCom;
-};
+//class speechCompetation {
+//public:
+//	
+//	speechCompetation(){
+//		fstream fs;
+//		fs.open("比赛记录.csv",ios::in);
+//		string num;
+//		getline(fs,num);
+//		this->numofCom = num[0]-48;
+//		for(int i=0;i<4*this->numofCom;i++){
+//			//TODO
+//			string temp;
+//			getline(fs,temp);
+//			if((i+4)%4==0) continue;
+//			for(int i=11;i<12;i++){
+//				//TODO
+//				cout<<temp[i]<<endl;
+//			}
+//			for(int i=12;i<temp.size();i++){
+//				//TODO
+//				cout<<temp[i]<<endl;
+//			}
+//		}
+//		fs.close();
+//	}
+//
+//	void competationStart() {
+//		vector<person> participants;
+//		personPrepare(participants);
+//		cout << "本届演讲比赛开始" << endl;
+//		cout << "-----------------------------------------" << endl;
+//		cout << "第一轮参赛选手出厂顺序如下：" << endl;
+//		random_shuffle(participants.begin(), participants.end());
+//		for_each(participants.begin(), participants.end(), printPerson);
+//		cout << "----------------------------------------" << endl;
+//		cout << "-------------比赛进行中-----------------" << endl;
+//		cout << "----------------------------------------" << endl;
+//		system("pause");
+//		cout << "----------------------------------------" << endl;
+//		for_each(participants.begin(), participants.end(), setScore);
+//		cout << "第一组排名顺序如下：" << endl;
+//		sort(participants.begin(), participants.begin() + 6,comparePerson);
+//		for_each(participants.begin(), participants.begin() + 6,printPerson);
+//		cout << "-----------------------------------------" << endl;
+//		cout << "第二组排名顺序如下：" << endl;
+//		sort(participants.begin() + 6, participants.end(), comparePerson);
+//		for_each(participants.begin() + 6, participants.end(), printPerson);
+//		cout << "----------------------------------------" << endl;
+//		cout << "-------------第二轮比赛进行中-----------" << endl;
+//		cout << "----------------------------------------" << endl;
+//		system("pause");
+//		participants.erase(participants.begin() + 9, participants.end());
+//		participants.erase(participants.begin() + 3, participants.begin()+6);
+//		for_each(participants.begin(), participants.end(), setScore);
+//		sort(participants.begin(), participants.end(), comparePerson);
+//		for_each(participants.begin(), participants.end(), printPerson);
+//		
+//		cout << "----------------------------------------" << endl;
+//		cout << "比赛结束，名次如下：" << endl;
+//		cout << "冠军是：" << participants[0].m_name << "\t得分：" << participants[0].m_score<<endl;
+//		cout << "亚军是：" << participants[1].m_name << "\t得分：" << participants[1].m_score<<endl;
+//		cout << "季军是：" << participants[2].m_name << "\t得分：" << participants[2].m_score<<endl;
+//		cout << "----------------------------------------" << endl;
+//		
+//		for (vector<person>::iterator it = participants.begin(); it != participants.end()-3;it++) {
+//			this->Honor.push_back(*it);
+//		}
+//		this->numofCom++;
+//		savePerson(this->Honor);
+//	}
+//	
+//	void lookup() {
+//		
+//		if (this->numofCom == 0) return ;
+//		for (int i = 0; i < this->numofCom; i++)
+//		{
+//			cout << "第" << i+1 << "届演讲比赛名次如下：" << endl;
+//			cout << "冠军是：" << this->Honor[0 + i * 3].m_name << "\t得分：" << this->Honor[0 + i * 3].m_score << endl;
+//			cout << "亚军是：" << this->Honor[1 + i * 3].m_name << "\t得分：" << this->Honor[1 + i * 3].m_score << endl;
+//			cout << "季军是：" << this->Honor[2 + i * 3].m_name << "\t得分：" << this->Honor[2 + i * 3].m_score << endl;
+//		}
+//	}
+//	
+//	void clearall() {
+//		cout << "是否清空比赛记录？" << endl;
+//		int choice;
+//		cin >> choice;
+//		if (choice) {
+//			this->Honor.clear();
+//			savePerson(this->Honor);
+//			this->numofCom=0;
+//		}
+//	
+//	}
+//public:
+//	vector<person> Honor;
+//	int numofCom;
+//};
 
 
 int main() {
